5196d83f7f6af266783d9494b4e93029f09b429f
nguyenquangsang2042 <nguyenquangsang2042@gmail.com>
2021-12-05 23:10:55 +0700

    basic dart init

---
+++ test/basic_consolose_syntax_test.dart
@@ -0,0 +1,8 @@
+import 'package:basic_consolose_syntax/basic_consolose_syntax.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
